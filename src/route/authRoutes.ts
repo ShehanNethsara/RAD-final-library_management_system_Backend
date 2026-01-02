86af23ca2b5dab40fb40e441011824a462bd8cfe
Shehan Nethsara <Shehan.nethsara2003@gmail.com>
2026-01-02 13:24:55 +0530

    update authroute

--- src/route/authRoutes.ts
+++ src/route/authRoutes.ts
@@ -3,14 +3,7 @@ import { registerUser, loginUser } from '../controller/authController';
 
 const router = express.Router();
 
-// @route   POST /api/auth/register
-// @desc    Register a new user
-// @access  Public
 router.post('/register', registerUser);
-
-// @route   POST /api/auth/login
-// @desc    Auth user & get token
-// @access  Public
 router.post('/login', loginUser);
 
 export default router;
